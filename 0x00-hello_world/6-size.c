5e29e3bec5b19eac92506c62b45b1159494052e4
Bashar Umar <basharu83@gmail.com>
2022-09-08 05:39:32 -0700

    Task 6

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,5 +1,4 @@
 #include <stdio.h>
-#include <string.h>
 /**
  *main - Entry point
  *Description:'Output the size of dataTypes'
@@ -8,10 +7,11 @@
 int main(void)
 {
 int integer_type;
+long integer_long;
 double double_type;
 float float_type;
 char char_type;
-printf("Size of int: %ld bytes\n", sizeof(integer_type));
+printf("Size of int: %ld bytes\n", sizeof(integer_long));
 printf("Size of float: %ld bytes\n", sizeof(float_type))
 printf("Size of double: %ld bytes\n", sizeof(double_type));
 printf("Size of char: %ld byte\n", sizeof(char_type));
