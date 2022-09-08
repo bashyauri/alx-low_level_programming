87ce00af03ebfbc82cd576aa1748bc3676754ae3
Bashar Umar <basharu83@gmail.com>
2022-09-08 11:10:40 -0700

    Task 8

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+*main-Entry Point
+*Description-'Print Error to the screen'
+*Return: Always (1) error
+*/
+int main(void)
+{
+  fprintf(stderr, "and that piece of art is useful\" - Dora Korpar, 2015-10-19");
+  return (1);
+}
