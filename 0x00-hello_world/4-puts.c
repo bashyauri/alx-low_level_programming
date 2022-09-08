4ff83b2d7f2fde1bc431189c17209c1b379f6ae6
Bashar Umar <basharu83@gmail.com>
2022-09-08 04:50:38 -0700

    Task 4

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,13 @@
+#!/bin/bash
+#include <stdio.h>
+#include <string.h>
+/**
+*main - Entry point
+*Description:'Output a string using puts command'
+*Return: Always 0 (Success)
+*/
+int main(void)
+{
+  puts("\"Programming is like building a multilingual puzzle");
+  return (0);
+}
