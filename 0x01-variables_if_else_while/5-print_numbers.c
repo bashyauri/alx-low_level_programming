6c50867f5b758f4d5284ab4ed5189d70affd203f
Bashar Umar <basharu83@gmail.com>
2022-09-09 06:35:26 -0700

    Task 5

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+*main - Entry point
+*Description:'output base 10 numbers'
+*Return: Always 0 (Success)
+*/
+int main(){
+int count =0;
+while(count < 10)
+printf("%d", count++);
+return 0;
+}
