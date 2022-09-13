5e2d3b207111cef63486bd9746b06616fa7f2dfb
Bashar Umar <basharu83@gmail.com>
2022-09-13 12:10:47 -0700

    Task 10

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+* add - adds two integrers and returns the result
+*
+* @x: number being added
+*
+* @y: number being added
+*
+* @sum: sum of two numbers
+*
+* Return: Always 0.
+*
+*/
+int add(int x, int y)
+{
+int sum = 0;
+sum = x + y;
+return (sum);
+}
