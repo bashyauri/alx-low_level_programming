fcc10e9ef1860b85fc1b50a261a8760568c23c56
Bashar Umar <basharu83@gmail.com>
2022-09-13 07:05:13 -0700

    Task 1

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,8 +1,10 @@
 #include "main.h"
 /**
-*main-Entry point
-*description-prints the alphabet in lowercase, followed by a new line.
-*Return:Always 0 (Success)
+* main - Entry point
+* 
+* Description: 'the program's description'
+* 
+* Return: Always 0 (Success)
 */
 void print_alphabet(void)
 {
