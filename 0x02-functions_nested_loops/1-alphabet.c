64d5152a01f84d9034e42ce345d8500375725fb5
Bashar Umar <basharu83@gmail.com>
2022-09-13 06:36:36 -0700

    Task 1

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -3,15 +3,10 @@
 *
 * Return: Always 0.
 */
-void print_alphabet()
+void print_alphabet(void)
 {
  char ch = 'a';
  for(ch = 'a'; ch<='z';++ch)
  _putchar(ch);
  _putchar('\n');
-}
-int main(void)
-{
-  print_alphabet();
-  return (0);
 }
