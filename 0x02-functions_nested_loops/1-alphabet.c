82b5fa61b4f7bbb921f8148747a08c9f3d344970
Bashar Umar <basharu83@gmail.com>
2022-09-13 06:52:01 -0700

    Task 1

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -2,9 +2,9 @@
 /**
 *main - Entry point
 * 
-*Description: 'prints the alphabet, in lowercase, followed by a new line'
+*Description:print_alphabet- 'prints the alphabet, in lowercase, followed by a new line'
 * 
-* Return: Always 0 (Success)
+*Return: Always 0 (Success)
 */
 void print_alphabet(void)
 {
