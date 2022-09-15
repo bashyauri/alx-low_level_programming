6dc5a4c81c55592388772ab37c9ebe5c2f22bbd9
Bashar Umar <basharu83@gmail.com>
2022-09-15 10:51:33 -0700

    Task 9

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -9,24 +9,17 @@ int i;
 for (i = 1; i <= 100; i++)
 {
 if (i % 15 == 0)
-
-printf("FizzBuzz ");
-
-
+printf("FizzBuzz");
+printf(" ");
 else if (i % 3 == 0)
-
-printf("Fizz ");
-
-
+printf("Fizz");
+printf(" ");
 else if (i % 5 == 0)
-
-printf("Buzz ");
-
-
+printf("Buzz");
+printf(" ");
 else
-
-printf("%d ", i);
-
+printf("%d", i);
+printf(" ");
 }
 printf("\n");
 return (0);
