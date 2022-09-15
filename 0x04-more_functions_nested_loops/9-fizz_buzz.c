07cd5b392d9036552f5e3210f139963812c0a79a
Bashar Umar <basharu83@gmail.com>
2022-09-15 10:14:08 -0700

    Task 9

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+/**
+* main - entry point
+*
+* Description: prints FizzBuzz
+*
+* Return: void
+*/
+int main() {
+  // Write C code here
+int i;
+for(int i = 1; i <= 100; i++)
+{
+if (i % 15 == 0)
+{
+printf("FizzBuzz ");
+
+}
+else if (i % 3 == 0)
+{
+printf("Fizz ");
+
+}
+else if (i % 5 == 0)
+{
+printf("Buzz ");
+
+}
+else
+{
+printf("%d ",i);
+}
+}
+
+return 0;
+}
