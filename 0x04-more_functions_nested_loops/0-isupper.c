ba4e90e77fef3e267f66a623258e3326213cfb98
Bashar Umar <basharu83@gmail.com>
2022-09-15 04:19:10 -0700

    Task 1

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+* _isupper - takes a date and prints how many days are
+* left in the year, taking leap years into account
+* @c: alphabet
+* @ch:characters
+* Return: (0)
+*/
+int int _isupper(int c)
+{
+  char ch;
+for (ch = 'A'; ch <= 'Z'; ch++)
+if(ch == c)
+return (1);
+return (0);
+  
+}
