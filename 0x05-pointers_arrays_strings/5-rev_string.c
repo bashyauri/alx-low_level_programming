9256ab57a9c9f05ca29d43a37a2e229f0da86855
Bashar Umar <basharu83@gmail.com>
2022-09-19 04:42:19 -0700

    Task 5

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+* rev_string - print an int numbers.
+* @s: number tested
+* Return: Always 0.
+*/
+void rev_string(char *s)
+{
+int i = 0;
+while(*(s + i))
+{
+i++;
+}
+for(i--; i >= 0; i--)
+putchar(*(s + i));
+}
+}
