c36f1d56e7921e08678d1c5e6e95648efd799649
Bashar Umar <basharu83@gmail.com>
2022-09-19 02:33:32 -0700

    Task 3

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+* print_number - prints a string, followed by a new line, to stdout.
+* @str: number tested
+* Return: Always 0.
+*/
+void _puts(char *str)
+{
+char* ch;
+int i;
+
+ch = str;
+
+for (i = 0; ch[i]; i++)
+{
+_putchar (c[i]);
+}
+_putchar('\n');
+}
