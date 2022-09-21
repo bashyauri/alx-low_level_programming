dfbc790b0f40d7384f33f10e3f6277748bec1242
Bashar Umar <basharu83@gmail.com>
2022-09-21 14:08:52 -0700

    Task 3

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -1,24 +1,23 @@
 #include "main.h"
 /**
-*_strcat - function that concatenates two strings.
-*@dest: pointer to destination string.
-*@src: pointer to source string.
-*
-*Return: pointer to destination string.
+ *_strcmp - compares two strings.
+ *@s1: pointer to first string.
+ *@s2: pointer to second string.
+ *
+ *Return: value less than 0 if string is less than the other.
+ *value greater than 0 if string is greater than the other.
+ *0 if strings are equal.
 */
-char *_strcat(char *dest, char *src)
+int _strcmp(char *s1, char *s2)
 {
-int count, i;
+int counter, cmpVal;
 
-count = 0;
-while (dest[count] != '\0')
+counter = 0;
+while (s1[counter] == s2[counter] && s1[counter] != '\0')
 {
-count++;
+counter++;
 }
-for (i = 0; src[i] != '\0'; i++, count++)
-{
-dest[count] = src[i];
-}
-dest[count] = '\0';
-return (dest);
+
+cmpVal = s1[counter] - s2[counter];
+return (cmpVal);
 }
