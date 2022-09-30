168f8ad1c336c0315a13ca4c463a97a76a50f651
Bashar Umar <basharu83@gmail.com>
2022-09-30 15:38:48 -0700

    Not feeling right

---
+++ 0x09-static_libraries/1-isdigit.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+* _isdigit - checks for a digit (0 through 9)
+* @c: digit
+* Return: (0)
+*/
+int _isdigit(int c)
+{
+int i;
+for (i = 48; i < 58; i++)
+if (c == i)
+return (1);
+return (0);
+}
